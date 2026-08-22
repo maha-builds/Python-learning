bad9e181344c5fc2658d3adcc10e042fa5214d16
MAHALAKSHMI.T <maha9751960852@gmail.com>
2026-08-22 09:21:31 +0530

    Add turtle graphics code to draw a heart shape

---
+++ Heart.py
@@ -0,0 +1,22 @@
+import turtle
+
+turtle.speed(3)
+turtle.color("red")
+turtle.fillcolor("red")
+
+turtle.begin_fill()
+
+turtle.left(140)
+turtle.forward(120)
+
+turtle.circle(-60, 200)
+
+turtle.left(120)
+
+turtle.circle(-60, 200)
+
+turtle.forward(120)
+
+turtle.end_fill()
+
+turtle.done()
